ec729b7768c0fa05474d5a7b66068c0c77152554
andrewhill <andrew.hill15@imperial.ac.uk>
2017-10-18 19:46:52 +0100

    loads in from csv

--- import-csv.php
+++ import-csv.php
@@ -6,19 +6,71 @@
  * Time: 02:04
  */
 
+$err = '';
+$msg = '';
+
 if ( isset($_POST["submit"]) ) {
 
-    if ( isset($_FILES["file"])) {
+    if (isset($_FILES["file"])) {
 
         $tmpName = $_FILES['file']['tmp_name'];
-        $csvAsArray = array_map('str_getcsv', file($tmpName));
+        $csv_array = array_map('str_getcsv', file($tmpName));
 
-        var_dump($csvAsArray);
+        /* CSV sanity check */
+        $num_cols = count($csv_array[0]);
+        $num_cols_in_union_csv = 13;
+        $num_rows = count($csv_array);
+
+
+        if ($num_cols == $num_cols_in_union_csv && $num_rows > 1) {
+
+            /* Import counter */
+            $count = 0;
+
+            /* Upload CSV to DB */
+            for ($i = 1; $i < $num_rows; $i++) {
+                //TODO: multiple row input in one sql statement
+                /* Query for all attendees matching the event */
+                $query = "INSERT INTO `qr_attendee`(`event_id`, `year`, `date`, `cid`, `login`, `first_name`, `surname`, `email`, `product_name`, `price`, `quantity_purchased`, `quantity_collected`) VALUES($event_id, " . $csv_array[$i][0] . "," . "'2017-09-21'" . "," . (int)$csv_array[$i][3] . ",'" . $csv_array[$i][4] . "','" . $csv_array[$i][5] . "','" . $csv_array[$i][6] . "','" . $csv_array[$i][7] . "','" . $csv_array[$i][8] . "'," . (int)$csv_array[$i][9] . "," . (int)$csv_array[$i][10] . "," . 0 . ")";                //STR_TO_DATE(?, '%m/%d/%Y')
+
+                /* prepare sql statement */
+                $stmt = $mysqli->prepare($query);
+                /*$stmt->bind_param('dssdsssssddd',  $event_id,
+                                                            $csv_array[$i][0], // year
+                                                            '2017-20-20',//$csv_array[$i][1], // date
+                                                            (int)$csv_array[$i][3], // cid
+                                                            $csv_array[$i][4], // login
+                                                            $csv_array[$i][5], // first_name
+                                                            $csv_array[$i][6], // surname
+                                                            $csv_array[$i][7], // email
+                                                            $csv_array[$i][8], // product_name
+                                                            (int)$csv_array[$i][9], // price
+                                                            (int)$csv_array[$i][10], // quantity purchased
+                                                            0 // quantity collected*/
+                //);
+
+                /* execute prepared statement */
+                if (!$stmt->execute()) {
+                    $err = "Database Error, please report to guilds@imperial.ac.uk";
+                    break;
+                }
+                $stmt->close();
+
+                $count++;
+                $msg = 'Successfully imported ' . $count . 'attendees. Refresh the page to upload more.';
+                unset($stmt);
+            }
+
+        } else {
+            $err = "No file selected, incompatible file selected, or upload failed";
+        }
 
     } else {
-        echo "No file selected <br />";
+        $err = "No file selected";
     }
 }
+
+
 
 
 
@@ -49,6 +101,9 @@ if ( isset($_POST["submit"]) ) {
             <br>
             <input class="btn btn-primary" type="submit" name="submit" />
         </form>
+        <br>
+        <?php echo $err ?>
+        <?php echo $msg ?>
     </div>
 </div>
 <!-- END EVENT TABLE LIST -->
