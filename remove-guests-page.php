0470ceba76cb4ced530d6e66baccc63f81e22b43
andrewhill <andrew.hill15@imperial.ac.uk>
2017-10-19 19:16:15 +0100

    remove guests: two new php files added, redirects based on url working, shows guestlist and remove links, remove links do nothing

---
+++ remove-guests-page.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrewhill
+ * Date: 19/10/2017
+ * Time: 19:10
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php echo_header_meta() ?>
+
+    <?php echo_header_scripts() ?>
+
+<!-- Page Title -->
+<title>Guestlist | QR Ticketing System :: CGCU</title>
+
+</head>
+
+<body>
+
+<?php echo_navbar(); ?>
+
+<div class="container well white-bkg" style="margin-top: 60px; position: relative">
+
+    <!-- Event Name and Date -->
+    <h2>
+        <?php echo $event_name ?>
+    </h2>
+    <h4>
+        <?php echo $event_date ?>
+    </h4>
+
+    <!-- EVENT TABLE LIST -->
+    <div class="">
+        <table class="table table-striped">
+            <colgroup>
+                <col class="col-md-1">
+                <col class="col-md-1">
+                <col class="col-md-1">
+                <col class="col-md-1">
+                <col class="col-md-3">
+                <col class="col-md-3">
+                <col class="col-md-1">
+                <col class="col-md-1">
+            </colgroup>
+            <thead>
+            <tr>
+                <th>CID</th>
+                <th>Login</th>
+                <th>First Name</th>
+                <th>Surname</th>
+                <th>Email</th>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Remove</th>
+            </tr>
+            </thead>
+
+            <tbody>
+
+            <?php
+            /* Query for all attendees matching the event */
+            $query = "SELECT id, cid, login, first_name, surname, email, product_name, price, quantity_purchased FROM qr_attendee WHERE event_id = " . (string)$event_id;
+
+            /* prepare sql statement */
+            $stmt = $mysqli->prepare($query);
+
+            /* execute prepared statement */
+            $stmt->execute();
+
+            /* get result obj */
+            $result = $stmt->get_result();
+
+            while ($row = $result->fetch_assoc()) {
+                echo '<tr>';
+                echo '<td>' . $row['cid'] . '</td>';
+                echo '<td>' . $row['login'] . '</td>';
+                echo '<td>' . $row['first_name'] . '</td>';
+                echo '<td>' . $row['surname'] . '</td>';
+                echo '<td>' . $row['email'] . '</td>';
+                echo '<td>' . $row['product_name'] . '</td>';
+                echo '<td>' . $row['price'] . '</td>';
+                echo '<td>' . $row['quantity_purchased'] . '</td>';
+                echo '<td> <a href="' . $row['id'] . '">Remove '. $row['first_name'] . '</a><td>';
+                echo '</tr>';
+            }
+
+            $stmt->close();
+            unset($stmt);
+            ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+<!-- END EVENT TABLE LIST -->
+
+<?php echo_footer() ?>
+
+<?php echo_footer_scripts() ?>
+
+</body>
+</html>
--- remove-guests.php
+++ remove-guests.php
@@ -5,3 +5,13 @@
  * Date: 16/10/2017
  * Time: 02:09
  */
+
+if(count($params) == 2 || count($params) == 3 && $params[2] == '') {
+    /* Display guestlist with remove button */
+    include 'remove-guests-page.php';
+} elseif (count($params) == 3 || count($params) == 4 && $params[3] == '') {
+    /* $params[3] contains the attendee ID of the row to delete */
+    include 'remove-guests-action.php';
+}
+
+?>
