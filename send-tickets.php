06270a8fb08df3697e5ba3cfc2ecfe8844fb0f54
andrewhill <andrew.hill15@imperial.ac.uk>
2017-10-20 02:37:30 +0100

    qr code is generated and displayed in base64 text on send-tickets
    (qr is only thing on the page)

--- send-tickets.php
+++ send-tickets.php
@@ -6,9 +6,17 @@
  * Time: 02:10
  */
 
+//include 'lib/phpqrcode/qrlib.php';
+include 'lib/phpqrcode.php';
+
+//$temp_file = tempnam(sys_get_temp_dir(), 'qr_');
+$temp_file = '/tmp/qr_F5k6FE';
+
+QRcode::png('code data text', $temp_file); // creates file
+
 /* Encode image into base 64 */
 /* Specify image path */
-$img_path = 'https://cgcu.net/files/vat.jpg';
+$img_path = $temp_file;
 
 /* Read image from path then convert to base64 encoding */
 $imgData = base64_encode(file_get_contents($img_path));
@@ -17,4 +25,6 @@ $imgData = base64_encode(file_get_contents($img_path));
 $src = 'data: '.mime_content_type($img_path).';base64,'.$imgData;
 
 /* Full img enclosed in html tags */
-echo '<img src="'.$src.'">';
+$qr_html_img = '<img src="'.$src.'">';
+
+echo $qr_html_img;
